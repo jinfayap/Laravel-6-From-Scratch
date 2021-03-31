b65c58ba38a10a341bd075503de306b3851530b2
jinfayap <jinfayap@gmail.com>
2021-04-01 00:35:27 +0800

    Hello Eloquent

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,17 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Post;
+use DB; // otherwise need providea backslash \DB::table
 // Can make use of 'php artisan make:controller PostsController' to generate the controller file
  
 
 class PostsController extends Controller
 {
     public function show($slug) {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
+
+        // $post = DB::table('posts')->where('slug', $slug)->first();
         
+        // $post = Post::where('slug', $slug)->firstOrFail();
+
         return view('post', [
-            'post' => $post
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,31 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* 
-    Wild card in {post}
-    Access wild card in the function callback.
-
-    Check 1array_key_exists to abort to 404 page (preferred)
-    or alternative return it as 
-    return view('post', [
-        'post' => $posts[$post] ?? 'Message here'
-    ]);
-
-*/
-Route::get('/posts/{post}', function ($post) {
-    $posts = [
-        'my-first-post' => 'Hello, this is my first blog post!',
-        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-    ];
-
-    if(!array_key_exists($post, $posts)) {
-        abort(404, 'Sorry, that post was not found');
-    }
-    return view('post', [
-        'post' => $posts[$post] 
-    ]);
-});
-
 // Laravel 6 - Route::get('/posts/{post}', 'PostsController@show');
 // But Laravel 8 change format to Route::get('/posts/{post}', 'App\Http\Controllers\PostsController@show');
 // Route::get('/posts/{post}', 'App\Http\Controllers\PostsController@show');
