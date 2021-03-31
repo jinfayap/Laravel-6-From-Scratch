3705a209cb20110cc6cbc2fc74fc2a1f4f1f0264
jinfayap <jinfayap@gmail.com>
2021-03-31 21:19:51 +0800

    Route Wildcards

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* 
+    Wild card in {post}
+    Access wild card in the function callback.
 
+    Check 1array_key_exists to abort to 404 page (preferred)
+    or alternative return it as 
+    return view('post', [
+        'post' => $posts[$post] ?? 'Message here'
+    ]);
 
-Route::get('test', function () {
-    return view('test', [
-        'name' => request('name')
+*/
+Route::get('/posts/{post}', function ($post) {
+    $posts = [
+        'my-first-post' => 'Hello, this is my first blog post!',
+        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
+    ];
+
+    if(!array_key_exists($post, $posts)) {
+        abort(404, 'Sorry, that post was not found');
+    }
+    return view('post', [
+        'post' => $posts[$post] 
     ]);
 });
+
+
