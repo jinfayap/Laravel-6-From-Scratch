ca4f8f31af8359faca050bd9b86047b4463b21d9
jinfayap <jinfayap@gmail.com>
2021-04-01 22:16:54 +0800

    Leverage Route Model Binding

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -13,8 +13,7 @@ class ArticlesController extends Controller
     }
 
     // Show a single resource
-    public function show($id) {
-        $article = Article::find($id);
+    public function show(Article $article) {
 
         return view('articles.show', [
             'article' => $article
@@ -48,23 +47,22 @@ class ArticlesController extends Controller
     }
 
     // Show a view to edit an existing resource
-    public function edit($id) {
+    public function edit(Article $article) {
         // find the article assoicated to the id in the uri
-        $article = Article::find($id);
-        
+
+ 
         return view('articles.edit', compact('article'));
     }
 
     // Persist the edited resource
-    public function update($id) {
+    public function update(Article $article) {
 
         request()->validate([
             'title' => 'required',
             'exerpt' => 'required',
             'body' => 'required'
         ]);
-        
-        $article = Article::find($id);
+
         $article->title = request('title');
         $article->exerpt = request('exerpt');
         $article->body = request('body');
